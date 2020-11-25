08fa3a7401977d6199af2ed8d5fc8926ff779273
Michael Ballard <ballard.michael9@gmail.com>
2020-11-25 02:00:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ function submitData(name, email){
             "Accept":"application/json"
         },
         body: JSON.stringify({
-            name:name,
-            email:email
+            name,
+            email
         })
     })
     .then(res => res.json())
