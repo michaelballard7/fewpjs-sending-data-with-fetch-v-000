ac21992c53bd95c1ab98f34c341d5ef7d61a8296
Michael Ballard <ballard.michael9@gmail.com>
2020-11-25 01:35:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,24 @@
 console.log('Hello World')
 
 let url = "http://localhost:3000/dog"
+
+// i can prewrite data and configs
+let formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle"
+};
+ 
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+
+// I can also write on the fly
+
 fetch(url, {
     method: "POST",
     headers: {
@@ -12,4 +30,4 @@ fetch(url, {
         dogName: "Byron",
         dogBreed: "Poodle"
     })
-})
+});
