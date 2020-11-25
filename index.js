235ac1fcd4375e17396fa066d4f8982d2800cdff
Michael Ballard <ballard.michael9@gmail.com>
2020-11-25 01:41:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,36 +1,75 @@
-// Add your code here
-console.log('Hello World')
 
-let url = "http://localhost:3000/dogs"
-
-// i can prewrite data and configs
-let formData = {
-  dogName: "Byron",
-  dogBreed: "Poodle"
-};
-
-let configObj = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
-};
-
-// I can also write on the fly
-
-fetch(url, {
-    method: "POST",
-    headers: {
-        "Content-Type":'application/json',
-        "Accept": "application/json"
-    },
-    body: JSON.stringify({
-        dogName: "Byron",
-        dogBreed: "Poodle"
+function submitData(name, email){
+    let url = "http://localhost:3000/users";
+    fetch(url, {
+        method:'POST',
+        headers: {
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body: JSON.stringify({
+            name:name,
+            email:email
+        })
     })
-})
-.then( res => res.json())
-.then( data => console.log(data))
-.catch(err => console.log(err))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log('Hello World')
+// 
+// let url = "http://localhost:3000/dogs"
+// 
+// // i can prewrite data and configs
+// let formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle"
+// };
+// 
+// let configObj = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify(formData)
+// };
+// 
+// // I can also write on the fly
+// 
+// fetch(url, {
+//     method: "POST",
+//     headers: {
+//         "Content-Type":'application/json',
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle"
+//     })
+// })
+// .then( res => res.json())
+// .then( data => console.log(data))
+// .catch(err => console.log(err))
